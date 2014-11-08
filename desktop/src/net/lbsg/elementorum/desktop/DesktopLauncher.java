582b5fab9c1700b06cb0a3d056ec99ec573d7b22
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-11-08 13:09:52 -0600

    Added libGDX files.

---
+++ desktop/src/net/lbsg/elementorum/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.lbsg.elementorum.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.lbsg.elementorum.Elementorum;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Elementorum(), config);
+	}
+}
