519ca7996a3f36d2a7167bc4c0c2069a3780eba2
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-11-08 17:22:41 -0600

    We need an assets directory.

--- core/src/net/lbsg/elementorum/worlds/Level.java
+++ core/src/net/lbsg/elementorum/worlds/Level.java
@@ -1,8 +1,12 @@
 package net.lbsg.elementorum.worlds;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import net.lbsg.elementorum.sprites.*;
 
 /**
  * @author Nicolás A. Ortega
@@ -14,15 +18,31 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
  * 
  */
 public class Level extends BaseScreen {
+	private TiledMap map;
+	private OrthographicCamera cam;
+	private TiledMapRenderer mapRenderer;
+	
+	private Player player;
+	
 	// Constructor:
 	public Level() {
 		super("Game");
+		
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.update();
+		map = new TmxMapLoader().load("gfx/lvl1.tmx");
+		mapRenderer = new OrthogonalTiledMapRenderer(map);
 	}
 	
 	// Update:
 	@Override
 	public void render(float delta) {
 		super.render(delta);
+		
+		cam.update();
+		mapRenderer.setView(cam);
+		mapRenderer.render();
 	}
 	
 	// Dispose:
