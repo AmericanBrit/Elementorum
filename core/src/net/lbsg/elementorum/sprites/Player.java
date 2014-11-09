cd2c6c0cff6ba643532fd2b12c3cf11445b2dc59
SkierEthan <ethanferry@icloud.com>
2014-11-08 18:36:19 -0600

    player v8
    Added directions

--- core/src/net/lbsg/elementorum/sprites/Player.java
+++ core/src/net/lbsg/elementorum/sprites/Player.java
@@ -8,24 +8,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Player extends Sprite{
 	private final float SPEED = 150f;
-	
+	private int direction;
 	public Player(String imgpath, float x, float y){
 		super(new Texture(imgpath));
-		
+		direction = 0;
 		setOrigin(x, y);
 	}
 	
 	public void render(float delta, SpriteBatch batch) {
 		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
 			translateX(-SPEED*delta);
+			direction = 1;
 		} else if(Gdx.input.isKeyPressed(Input.Keys.D)) {
 			translateX(SPEED*delta);
+			direction = 3;
 		} else if(Gdx.input.isKeyPressed(Input.Keys.W)){
 			translateY(SPEED*delta);
+			direction = 0;
 		} else if(Gdx.input.isKeyPressed(Input.Keys.S)){
 			translateY(-SPEED*delta);
+			direction = 2;
 		}
 		
 		super.draw(batch);
+	}
+	
+	public int getDirection(){
+		return direction;
+	
 	}
 }
